b29a0d2d8ca8a96e513c4ff3a82d02e0a8587707
raihan2bd <raihan2bd.official@gmail.com>
2023-07-12 22:19:48 +0600

    Fix linters errors from this project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-const App = () => {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>Hello World!</h1>
+  </div>
+);
 
 export default App;

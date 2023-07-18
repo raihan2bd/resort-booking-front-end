ce67e3666961da83dcdd0bcf743d37a87f615791
raihan2bd <raihan2bd.official@gmail.com>
2023-07-18 18:08:50 +0600

    Fix linters errors from the project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router} from 'react-router-dom'
-import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import store from './redux/store';
 import App from './App';
--- src/redux/my-reservations/my-reservations.js
+++ src/redux/my-reservations/my-reservations.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  myReservations: []
+  myReservations: [],
 };
 
 const myReservationsSlice = createSlice({

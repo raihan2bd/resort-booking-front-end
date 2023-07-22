4ae330a8dac41586f508e12353bbba6bad71bc02
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-22 13:59:00 +0100

    Add reservation form components and location option

--- src/components/ReservationForms.js
+++ src/components/ReservationForms.js
@@ -1,3 +1,26 @@
 import { useState } from "react";
 import { fetchLogin } from "../../redux/auth/authSlice";
-import { UseSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from 'prop-types';
+
+const ReservationForm = ({selectedResortId, resortSelected}) => {
+  console.log(selectedResortId);
+  console.log(resortSelected);
+  const fullname = localStorage.getItem('name');
+  const dispatch = useDispatch();
+  const resorts = useSelector((state) => state.resorts.resorts) || [];
+
+  const [formData, setFormData] = useState({
+    city: '',
+    date: '',
+    resort_id: '',
+    start_date: '',
+    end_date: ''
+  });
+  const locations = [
+    { id: 1, name: 'Madrid' },
+    { id: 2, name: 'Capetown' },
+  ];
+
+  
+}

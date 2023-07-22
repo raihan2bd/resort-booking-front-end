f5ffe4e06cff37d3bd4288112c989c233e3788c6
MICHAELKITH <michaelkithinji27@gmail.com>
2023-07-22 11:49:44 +0300

    Creating navbar

---
+++ src/components/BookingItem/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = () =>  {
+  return (
+    <header>main</header>
+    <main>
+        <aside>
+
+        </aside>
+        <div>{prop}</div>
+    </main>
+
+  )
+}
+
+export default Layout

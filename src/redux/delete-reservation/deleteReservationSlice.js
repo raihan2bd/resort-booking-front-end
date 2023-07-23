d178bbd9af6f39f17fd981ebcad8f1dc13120d31
edubew <edubew98@gmail.com>
2023-07-23 03:13:04 +0300

    Update async methods to use api url

--- src/redux/delete-reservation/deleteReservationSlice.js
+++ src/redux/delete-reservation/deleteReservationSlice.js
@@ -1,29 +1,61 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async () => {
-    const response = await axios.get('/reservations', { withCredentials: true });
+const url = 'http://localhost:4000';
+
+export const fetchReservations = createAsyncThunk('reservations/fetchReservations', async (_, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(`${url}/reservations`, { withCredentials: true });
     return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
 });
 
-export const deleteReservation = createAsyncThunk('reservations/deleteReservation', async (bookingId) => {
-    const response = await axios.delete(`/reservations/${bookingId}`, { withCredentials: true });
+export const deleteReservation = createAsyncThunk('reservations/deleteReservation', async (userId, { rejectWithValue }) => {
+  try {
+    const response = await axios.delete(`${url}/reservations/${userId}`, { withCredentials: true });
     return response.data;
+  } catch (error) {
+    return rejectWithValue(error.response.data);
+  }
 });
 
-const deleteReservationsSlice = createSlice({
-    name: 'reservations',
-    initialState: [],
-    reducers: {},
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchReservations.fulfilled, (state, action) => {
-          return action.payload;
-        })
-        .addCase(deleteReservation.fulfilled, (state, action) => {
-          return state.filter((reservation) => reservation.id !== action.payload.id);
-        });
-    },
-  });
-  
-  export default deleteReservationsSlice.reducer;
+const deletereservationsSlice = createSlice({
+  name: 'reservations',
+  initialState: {
+    reservations: [],
+    status: 'idle',
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchReservations.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(fetchReservations.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.reservations = action.payload;
+      })
+      .addCase(fetchReservations.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload;
+      })
+      .addCase(deleteReservation.pending, (state) => {
+        state.status = 'loading';
+        state.error = null;
+      })
+      .addCase(deleteReservation.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.reservations = state.reservations.filter((reservation) => reservation.id !== action.payload.id);
+      })
+      .addCase(deleteReservation.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.payload;
+      });
+  },
+});
+
+export default deletereservationsSlice.reducer;

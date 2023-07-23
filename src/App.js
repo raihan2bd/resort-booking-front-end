85429473dd37f02b084f10d3f28106f791345de0
edubew <edubew98@gmail.com>
2023-07-23 11:11:05 +0300

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -8,18 +8,6 @@ import MyBookingsPage from './pages/MyBookingsPage';
 import RegistrationForm from './pages/RegistrationForm';
 import SignInForm from './pages/SignInForm';
 import DeleteReservation from './pages/DeleteReservation';
-
-const App = () => (
-  <>
-    <Routes>
-      <Route path="/my-bookings" element={<MyBookingsPage />} />
-      <Route path="/delete-reservations" element={<DeleteReservation />} />
-      <Route path="/register" element={<RegistrationForm />} />
-      <Route path="/signIn" element={<SignInForm />} />
-    </Routes>
-  </>
-);
-
 import MainPage from './pages/MainPage';
 import AuthSpinner from './components/UI/AuthSpinner';
 
@@ -54,6 +42,7 @@ const App = () => {
           element={!auth.isAuth ? <SignInForm /> : <Navigate to="/" />}
         />
         <Route path="/" element={<MainPage />} />
+        <Route path="/delete-reservations" element={<DeleteReservation />} />
       </Routes>
     </>
   );

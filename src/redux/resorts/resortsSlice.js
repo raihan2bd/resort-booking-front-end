164507d197c2a15ecc7b8c41ee73ba3dde1e059a
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-24 08:56:12 +0100

    Create state for resorts in redux

---
+++ src/redux/resorts/resortsSlice.js
@@ -0,0 +1,49 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const RESORTS = 'resorts'
+
+const initialState = {
+  resorts: [],
+  loading: true,
+  error: false
+};
+
+export const fetchResorts = createAsyncThunk(
+  RESORTS,
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = await axios.post('http://127.0.0.1:4000/resorts');
+      return { resorts: res.data };
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  },
+
+  
+);
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case `${RESORTS}/pending`:
+      return {
+        loading: true,
+        resorts: [],
+        error: false
+      };
+    case `${RESORTS}/fulfilled`:
+      return {
+        loading: false,
+        resorts: action.payload,
+        error: false
+      };
+    case `${RESORTS}rejected`:
+      return {
+        loading: false,
+        resorts: [],
+        error: true
+      };
+    default:
+      return { ...state };
+  }
+};

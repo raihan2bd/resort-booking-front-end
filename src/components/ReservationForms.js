360df31e2b8d6507cabd15725ee280dac349765f
lawrahkonwea <lawrahcollections@gmail.com>
2023-07-24 18:06:30 +0100

    Fix errors

--- src/components/ReservationForms.js
+++ src/components/ReservationForms.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-// import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchResorts } from '../redux/resorts/resortsSlice';
 import { createBookings } from '../redux/reservation/bookingsSlice';
